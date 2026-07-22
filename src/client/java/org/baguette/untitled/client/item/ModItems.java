ab12b7811dfa5caab65c3edcc15c279c1bd9036b
ItsMimbo <ItsMimbo@outlook.com>
2026-07-22 23:08:25 +0200

    added mod icon.

---
+++ src/client/java/org/baguette/untitled/client/item/ModItems.java
@@ -0,0 +1,9 @@
+package org.baguette.untitled.client.item;
+
+import org.baguette.untitled.Untitled;
+
+public class ModItems {
+    public static void registerModItems() {
+        Untitled.LOGGER.info("Registering Mod Items for " + Untitled.MOD_ID);
+    }
+}
