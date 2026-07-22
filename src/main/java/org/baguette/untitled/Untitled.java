604c50cadbf09eb9f1fd3341815e0d26e6ae79ea
ItsMimbo <ItsMimbo@outlook.com>
2026-07-22 23:44:14 +0200

    fixed MOD_ID and LOGGER.

--- src/main/java/org/baguette/untitled/Untitled.java
+++ src/main/java/org/baguette/untitled/Untitled.java
@@ -1,8 +1,12 @@
 package org.baguette.untitled;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Untitled implements ModInitializer {
+    public static final String MOD_ID = "untitled";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
