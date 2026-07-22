047a866ea56aa052896b74e87e41d52f80a464f5
ItsMimbo <ItsMimbo@outlook.com>
2026-07-22 23:09:31 +0200

    fixed some shit.

--- src/main/java/org/baguette/untitled/Untitled.java
+++ src/main/java/org/baguette/untitled/Untitled.java
@@ -7,11 +7,4 @@ public class Untitled implements ModInitializer {
     @Override
     public void onInitialize() {
     }
-
-    public class ModItemIds {
-        public static ResourceKey<Item> create(String name) {
-            // Create the item key.
-            return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(Untitled.MOD_ID, name));
-        }
-    }
 }
