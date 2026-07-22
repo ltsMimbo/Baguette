4227e967930d3babce19d7637996657ec7de2ff0
ItsMimbo <ItsMimbo@outlook.com>
2026-07-22 22:34:07 +0200

    made the mod a mod.

---
+++ src/client/java/org/baguette/untitled/client/UntitledClient.java
@@ -0,0 +1,10 @@
+package org.baguette.untitled.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UntitledClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/baguette/untitled/Untitled.java
@@ -0,0 +1,10 @@
+package org.baguette.untitled;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Untitled implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
