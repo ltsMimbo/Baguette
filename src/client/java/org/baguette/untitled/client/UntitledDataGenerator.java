8e6c45bc21e6adb2f36fd66cdb0661756992b5bf
ItsMimbo <ItsMimbo@outlook.com>
2026-07-23 13:20:07 +0200

    added datagen (yay).

--- src/client/java/org/baguette/untitled/client/UntitledDataGenerator.java
+++ src/client/java/org/baguette/untitled/client/UntitledDataGenerator.java
@@ -2,11 +2,14 @@ package org.baguette.untitled.client;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.baguette.untitled.client.datagen.ModModelProvider;
 
 public class UntitledDataGenerator implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(ModModelProvider::new);
     }
 }
